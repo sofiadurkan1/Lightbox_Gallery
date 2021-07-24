3095242f464f4dd5f2fb9e97046c13ad5bf47b99
sofia durkan <sofiadurkan@gmail.com>
2021-07-23 21:27:05 -0500

    completed

--- app.js
+++ app.js
@@ -1,67 +1,75 @@
 //selecting all required elements
 
-const gallery  = document.querySelectorAll(".image"),
-previewBox = document.querySelector(".preview-box"),
-previewImg = previewBox.querySelector("img"),
-closeIcon = previewBox.querySelector(".icon"),
-currentImg = previewBox.querySelector(".current-img"),
-totalImg = previewBox.querySelector(".total-img"),
+const gallery = document.querySelectorAll(".gallery .image");
+previewBox = document.querySelector(".preview-box");
+previewImg = previewBox.querySelector("img");
+closeIcon = previewBox.querySelector(".icon");
+currentImg = previewBox.querySelector(".current-img");
+totalImg = previewBox.querySelector(".total-img");
 shadow = document.querySelector(".shadow");
-window.onload = ()=>{
+
+window.onload = () => {
     for (let i = 0; i < gallery.length; i++) {
-        totalImg.textContent = gallery.length; 
-        let newIndex = i; //passing i value to newIndex variable
-        let clickedImgIndex; //creating new variable
-        
-        gallery[i].onclick = () =>{
-            clickedImgIndex = i; 
-            function preview(){
-                currentImg.textContent = newIndex + 1; //passing current img index to currentImg varible with adding +1
-                let imageURL = gallery[newIndex].querySelector("img").src; //getting user clicked img url
-                previewImg.src = imageURL; //passing user clicked img url in previewImg src
+        totalImg.textContent = gallery.length;
+        let newIndex = i;
+        let clickImgIndex;
+        gallery[i].onclick = () => {
+            // currentImg.textContent = newIndex + 1;
+            clickImgIndex = newIndex;
+            console.log(i);
+            function preview() {
+                currentImg.textContent = newIndex + 1;
+                let selectedImgUrl = gallery[newIndex].querySelector("img").src;
+                previewImg.src = selectedImgUrl;
+                // console.log(selectedImgUrl);
             }
-            preview(); //calling above function
-    
+
             const prevBtn = document.querySelector(".prev");
             const nextBtn = document.querySelector(".next");
-            if(newIndex == 0){ //if index value is equal to 0 then hide prevBtn
-                prevBtn.style.display = "none"; 
+            if (newIndex == 0) {
+                prevBtn.style.display = "none";
             }
-            if(newIndex >= gallery.length - 1){ //if index value is greater and equal to gallery length by -1 then hide nextBtn
-                nextBtn.style.display = "none"; 
+            if (newIndex >= gallery.length - 1) {
+                nextBtn.style.display = "none";
             }
-            prevBtn.onclick = ()=>{ 
-                newIndex--; //decrement index
-                if(newIndex == 0){
-                    preview(); 
-                    prevBtn.style.display = "none"; 
-                }else{
+            prevBtn.onclick = () => {
+                newIndex--;
+                if (newIndex == 0) {
+                    preview();
+                    prevBtn.style.display = "none";
+                }else {
                     preview();
                     nextBtn.style.display = "block";
-                } 
+                }
             }
-            nextBtn.onclick = ()=>{ 
-                newIndex++; //increment index
-                if(newIndex >= gallery.length - 1){
-                    preview(); 
+
+            nextBtn.onclick = () => {
+                newIndex++;
+                if (newIndex >= gallery.length - 1) {
+                    preview();
                     nextBtn.style.display = "none";
-                }else{
-                    preview(); 
+                }else {
+                    preview();
                     prevBtn.style.display = "block";
                 }
             }
+
+
+
+            preview();
+            previewBox.classList.add("show");
+            shadow.style.display = "block";
             document.querySelector("body").style.overflow = "hidden";
-            previewBox.classList.add("show"); 
-            shadow.style.display = "block"; 
-            closeIcon.onclick = ()=>{
-                newIndex = clickedImgIndex; //assigning user first clicked img index to newIndex
-                prevBtn.style.display = "block"; 
+            
+
+            closeIcon.onclick = () => {
+                newIndex = clickImgIndex;
+                prevBtn.style.display = "block";
                 nextBtn.style.display = "block";
                 previewBox.classList.remove("show");
                 shadow.style.display = "none";
-                document.querySelector("body").style.overflow = "scroll";
+                document.querySelector("body").style.overflow = "auto";
             }
         }
-        
-    } 
+    }
 }
